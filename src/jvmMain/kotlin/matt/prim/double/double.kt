130d28c26acbd7f9a633ae27f47dfd55ad269fa1
mgroth0 <mgroth49@gmail.com>
2023-01-31 23:30:44 -0500

    autocommit

--- src/jvmMain/kotlin/matt/prim/double/double.kt
+++ src/jvmMain/kotlin/matt/prim/double/double.kt
@@ -5,3 +5,5 @@ import java.nio.ByteBuffer
 const val DOUBLE_BYTE_LEN = 8
 
 fun Double.toByteArray() = ByteBuffer.allocate(DOUBLE_BYTE_LEN).putDouble(this).array()
+
+fun Double.isNaNInfiniteOrZero() = isNaN() || isInfinite() || this == 0.0
--- src/jvmMain/kotlin/matt/prim/float/float.kt
+++ src/jvmMain/kotlin/matt/prim/float/float.kt
@@ -5,3 +5,5 @@ import java.nio.ByteBuffer
 const val FLOAT_BYTE_LEN = 4
 
 fun Float.toByteArray() = ByteBuffer.allocate(FLOAT_BYTE_LEN).putFloat(this).array()
+
+fun Float.isNaNInfiniteOrZero() = isNaN() || isInfinite() || this == 0f
