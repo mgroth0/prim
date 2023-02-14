06706e5f4d7941aae3c764d2a3d28c32e1a48383
mgroth0 <mgroth49@gmail.com>
2023-02-14 13:52:22 -0500

    autocommit

--- src/commonMain/kotlin/matt/prim/str/str.kt
+++ src/commonMain/kotlin/matt/prim/str/str.kt
@@ -3,6 +3,8 @@ package matt.prim.str
 import matt.prim.str.mybuild.string
 import kotlin.random.Random
 
+fun String.removePrefixAndOrSuffix(s: String) = removePrefix(s).removeSuffix(s)
+
 fun String.remove(s: String) = replace(s, "")
 
 fun String.shuffled(rand: Random = Random): String {
