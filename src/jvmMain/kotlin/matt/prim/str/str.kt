d7325b51e41fa401a51c2b82bc59afe7627e45bd
mgroth0 <mgroth49@gmail.com>
2023-02-16 17:54:28 -0500

    autocommit

--- src/jvmMain/kotlin/matt/prim/str/str.kt
+++ src/jvmMain/kotlin/matt/prim/str/str.kt
@@ -3,6 +3,6 @@ package matt.prim.str
 import java.net.URLEncoder
 
 /*https://stackoverflow.com/questions/4737841/urlencoder-not-able-to-translate-space-character*/
-fun String.urlEncodePlusNotPercentForSpaces() = URLEncoder.encode(
+fun String.urlEncode() = URLEncoder.encode(
   this, Charsets.UTF_8
 ).replace("+", "%20")
