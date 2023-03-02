caf2838cacd964303bb0c37acada0edabb3ab871
mgroth0 <mgroth49@gmail.com>
2023-03-02 00:17:30 -0500

    autocommit

--- src/jvmMain/kotlin/matt/prim/double/double.kt
+++ src/jvmMain/kotlin/matt/prim/double/double.kt
@@ -1,9 +1,20 @@
 package matt.prim.double
 
 import java.nio.ByteBuffer
+import kotlin.math.ceil
+import kotlin.math.floor
 
 const val DOUBLE_BYTE_LEN = 8
 
 fun Double.toByteArray() = ByteBuffer.allocate(DOUBLE_BYTE_LEN).putDouble(this).array()
 
 fun Double.isNaNInfiniteOrZero() = isNaN() || isInfinite() || this == 0.0
+
+fun Double.isWholeNumber(): Boolean {
+  return ceil(this) == floor(this)
+}
+
+fun Double.requireIsWholeNumber(): Double {
+  require(isWholeNumber())
+  return this
+}
--- src/jvmMain/kotlin/matt/prim/int/int.kt
+++ src/jvmMain/kotlin/matt/prim/int/int.kt
@@ -3,3 +3,5 @@ package matt.prim.int
 import java.nio.ByteBuffer
 
 fun Int.toByteArray() = ByteBuffer.allocate(4).putInt(this).array()
+
+
